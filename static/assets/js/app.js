844c9d86ac4c68d182c0f1b08498ca2088875f84
Bayrex <bayrexr@gmail.com>
2019-01-20 01:25:24 -0500

    Adds functional metadata section that updates on new selections

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -5,7 +5,7 @@ function buildPlots() {
     d3.json(defaultUrl).then(function(data){
         let pie_data = [data[0].pie_data[0]];
         let bubble_data = [data[0].bubble_data[0]];
-        let meta_data = [data[0].metaData];
+        let meta_data = [data[0].metadata[0]];
 
         console.log(data);
         //Plots
@@ -13,7 +13,12 @@ function buildPlots() {
         Plotly.newPlot("bubble", bubble_data);
 
         //Metadata
-
+        d3.select("#age").text(`Age: ${meta_data[0].AGE}`);
+        d3.select("#bb_type").text(`BBTYPE: ${meta_data[0].BBTYPE}`);
+        d3.select("#ethnicity").text(`Ethnicity: ${meta_data[0].ETHNICITY}`);
+        d3.select("#gender").text(`Gender: ${meta_data[0].GENDER}`);
+        d3.select("#location").text(`Location: ${meta_data[0].LOCATION}`);
+        d3.select("#sample_id").text(`Sample ID: ${meta_data[0].sample}`);
     });
 };
 
@@ -25,7 +30,7 @@ function update(sample) {
     d3.json(url).then(function(data){
         let pie_data = [data[0].pie_data[0]];
         let bubble_data = [data[0].bubble_data[0]];
-        let meta_data = [data[0].metaData];
+        let meta_data = [data[0].metadata[0]];
 
         console.log(data);
         updatePlots(pie_data, bubble_data, meta_data);
@@ -34,18 +39,25 @@ function update(sample) {
 
 //Update plots
 function updatePlots(pie_data, bubble_data, meta_data) {
+
     //Restyle Pie
-    Plotly.restyle("pie", "values", [pie_data.values]);
-    Plotly.restyle("pie", "labels", [pie_data.labels]);
-    Plotly.restyle("pie", "hoverinfo", [pie_data.hoverinfo]);
+    Plotly.restyle("pie", "values", [pie_data[0].values]);
+    Plotly.restyle("pie", "labels", [pie_data[0].labels]);
+    Plotly.restyle("pie", "hoverinfo", [pie_data[0].hoverinfo]);
 
     //Restyle Bubble
-    Plotly.restyle("bubble", "x", [bubble_data.x]);
-    Plotly.restyle("bubble", "y", [bubble_data.y]);
-    Plotly.restyle("bubble", "marker", [bubble_data.marker]);
-    Plotly.restyle("bubble", "text", [bubble_data.text]);
+    Plotly.restyle("bubble", "x", [bubble_data[0].x]);
+    Plotly.restyle("bubble", "y", [bubble_data[0].y]);
+    Plotly.restyle("bubble", "marker", [bubble_data[0].marker]);
+    Plotly.restyle("bubble", "text", [bubble_data[0].text]);
 
     //Update Meta
+    d3.select("#age").text(`Age: ${meta_data[0].AGE}`);
+    d3.select("#bb_type").text(`BBTYPE: ${meta_data[0].BBTYPE}`);
+    d3.select("#ethnicity").text(`Ethnicity: ${meta_data[0].ETHNICITY}`);
+    d3.select("#gender").text(`Gender: ${meta_data[0].GENDER}`);
+    d3.select("#location").text(`Location: ${meta_data[0].LOCATION}`);
+    d3.select("#sample_id").text(`Sample ID: ${meta_data[0].sample}`);
 };
 
 buildPlots();
